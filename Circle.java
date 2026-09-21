88d9a1bc23adfdb493a01186efe56aa7b606ba69
nikitadangi093-source <nikitadangi093@gmail.com>
2026-09-21 13:42:34 +0530

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,22 @@
+class Circle {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double area() {
+        return Math.PI * radius * radius;
+    }
+
+    double circumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+        Circle c = new Circle(5);
+
+        System.out.println("Area = " + c.area());
+        System.out.println("Circumference = " + c.circumference());
+    }
+}
